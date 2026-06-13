07a854ecdfe4040ae2db74f82976e1309f820e74
Nitesh pandey <140426447+Niteshpandey80@users.noreply.github.com>
2026-06-13 15:30:54 +0530

    adding ui in this product

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Result from './pages/Result'
+import BuyCredit from './pages/BuyCredit'
 
 const App = () => {
   return (
     <div>
-      <h1>Hello Bro </h1>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/result' element={<Result/>}/>
+        <Route path='/buy' element={<BuyCredit/>}/>
+      </Routes>
     </div>
   )
 }
