2b7fcf88f1e392be0d1a94d5c91e9d2a7e7b2b0a
Nitesh pandey <140426447+Niteshpandey80@users.noreply.github.com>
2026-06-20 18:12:15 +0530

    adding Backend in my projecct

---
+++ server/index.js
@@ -0,0 +1,16 @@
+import 'dotenv/config'
+import express from 'express'
+import cors from 'cors'
+
+const PORT = process.env.PORT||4000
+const app = express()
+
+app.use(express.json());
+app.use(cors())
+
+app.get('/' , (req,res)=>{
+    res.send("Api Working")
+})
+app.listen(PORT , ()=>
+console.log("Runing on Port " + PORT)
+)
