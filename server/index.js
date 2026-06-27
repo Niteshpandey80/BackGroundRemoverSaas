06f3834c800979040686c84cf6d1d32f629ca46a
Nitesh pandey <140426447+Niteshpandey80@users.noreply.github.com>
2026-06-27 23:52:46 +0530

    adding things

--- server/index.js
+++ server/index.js
@@ -1,16 +1,12 @@
 import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
+import connectDB from './configs/mongodb.js'
 
 const PORT = process.env.PORT||4000
 const app = express()
 
 app.use(express.json());
 app.use(cors())
+await connectDB()
 
-app.get('/' , (req,res)=>{
-    res.send("Api Working")
-})
-app.listen(PORT , ()=>
-console.log("Runing on Port " + PORT)
-)
